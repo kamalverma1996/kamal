064f2309c66e5599f73022543084cf42c75609f7
kamalverma1996 <46883699+kamalverma1996@users.noreply.github.com>
2019-01-21 12:25:04 +0530

    Create hello.cs

---
+++ hello.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is kamal");
+            Console.ReadKey();
+        }
+    }
+}
